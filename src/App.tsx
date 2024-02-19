5f726752c4442991c9f2809c5b0414f27c10c888
Karen Almeida <karenalmeida.lukiany@gmail.com>
2024-02-19 20:45:38 +0000

    Initial commit + req. 1 complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Table from './components/Table';
+import { ApiDataType } from './types';
 
 function App() {
+  const [apiData, setApiData] = useState<ApiDataType[]>([]);
+
+  useEffect(() => {
+    fetch('https://swapi.dev/api/planets')
+      .then((response) => response.json())
+      .then((data) => setApiData(data.results));
+  }, []);
+
   return (
-    <span>Hello, App!</span>
+    <>
+      <h1>Star Wars</h1>
+      <Table planetesInfo={ apiData } />
+    </>
   );
 }
 
