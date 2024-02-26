bfa01f20ef21b46f380db5776bde483425e0dbf0
Karen Almeida <karenalmeida.lukiany@gmail.com>
2024-02-26 11:35:41 +0000

    requisito 5 aprovado

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import App from '../App';
+import { render, screen } from '@testing-library/react';
+import { vi } from 'vitest';
+import App from '../App';
+import StarWarsProvider from '../context/StarWarsProvider';
+import { mockData } from './mockData';
 
-test('I am your test', () => {
-  const I = "your father";
-  expect(I).toBe("your father");
+const MOCK_RESPONSE = {
+  ok: true,
+  status: 200,
+  json: async () => mockData,
+} as Response;
+
+describe('Table component', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  test('1. Verifica se informações, vindas da api, do componente "Table" renderizam corretamente.', async () => {
+    const fetchMock = vi.spyOn(global, 'fetch').mockResolvedValue(MOCK_RESPONSE);
+    render(<StarWarsProvider><App /></StarWarsProvider>);
+
+    const planetsName = await screen.findAllByTestId('planet-name');
+    expect(planetsName[0]).toBeInTheDocument();
+    
+    expect(fetchMock).toHaveBeenCalledTimes(1);
+    expect(mockData.results.length).toBe(10);
+  });
 });
